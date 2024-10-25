3b8ea268370f3f4734a54068edc4b5867b94f3e8
rolandfbk <rolandfbk@gmail.com>
2024-10-25 13:25:05 +0200

    person model set done

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,2 +1,6 @@
 class Person < ApplicationRecord
+  validates :name, :age, :category, presence: true
+  validates :name, length: { minimum: 50 }
+  validates :age, numericality: { only_integer: true }
+  validates :category, inclusion: { in: %w(man woman teenager kid baby) }
 end
