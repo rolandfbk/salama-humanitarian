593e3d78fb60006f07565368faabce3475c64edf
rolandfbk <rolandfbk@gmail.com>
2024-10-27 12:35:16 +0200

    first name and last name model validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,10 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  validates :first_name, :last_name, presence: true
+  validates :email, presence: true, uniqueness: true
+  validates :password, presence: true, length: { minimum: 6 }
+
   # Role constants
   ROLES = %w[admin volunteer].freeze
 
